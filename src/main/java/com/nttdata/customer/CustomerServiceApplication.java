866edb49bc92ad75359b54632de0b38e5d4a6c3b
paredesdaniel1721 <paredesdaniel1721@gmail.com>
2022-05-13 01:32:08 -0500

    modificando config server

--- src/main/java/com/nttdata/customer/CustomerServiceApplication.java
+++ src/main/java/com/nttdata/customer/CustomerServiceApplication.java
@@ -3,11 +3,17 @@ package com.nttdata.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+		/*SpringApplication app = new SpringApplication(CustomerServiceApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8091"));
+		app.run(args);*/
 	}
 
 }
